ea824b58ed41690b83ad563af7337e022e63d440
Naveen Katari <naveen.katari@sourcebits.com>
2015-10-06 12:44:59 +0530

    Initial Commit

---
+++ Area/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Area
+//
+//  Created by Naveen Katari on 06/10/15.
+//  Copyright (c) 2015 Sourcebits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Area/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Area
+//
+//  Created by Naveen Katari on 06/10/15.
+//  Copyright (c) 2015 Sourcebits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
